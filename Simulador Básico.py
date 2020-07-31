d78b8f1517c5538139b798cda51b59eb3c220e3a
Sérgio <64284595+SergioSantos801@users.noreply.github.com>
2020-07-31 19:18:12 -0300

    correção

--- Simulador Básico.py
+++ Simulador Básico.py
@@ -1,4 +1,4 @@
- import random
+import random
 lista1 = range (1, 60)
 list2 = range (1, 60)
 lista3 = range (1, 60)
@@ -6,5 +6,5 @@ lista4 = range (1, 60)
 lista5 = range (1, 60)
 lista6 = range (1, 60)
 escolha = random.shuffle(random.choice(lista1, lista2, lista3, lista4, lista5, lista6)
-print('Simulação concluida!')
+print('Simulação concluida!')) 
 print('escolha')
