621e0dfbc5418b97c42089ae350bd8016aeab4dc
Sérgio <64284595+SergioSantos801@users.noreply.github.com>
2020-07-31 19:35:45 -0300

    new sim.

---
+++ Simulador 2.py
@@ -0,0 +1,25 @@
+import random
+
+lista0 = range (1, 60)
+escolha = random.choice (lista0)
+print('1ª escolha --> {}' .format(escolha, lista0))
+
+lista1 = range (1, 60)
+escolha = random.choice (lista1)
+print('2ª escolha --> {}' .format(escolha, lista1))
+
+lista2 = range (1, 60)
+escolha = random.choice (lista2)
+print('3ª escolha --> {}' .format(escolha, lista2))
+
+lista3 = range (1, 60)
+escolha = random.choice (lista3)
+print('4ª escolha --> {}' .format(escolha, lista3))
+
+lista4 = range (1, 60)
+escolha = random.choice (lista4)
+print('5ª escolha --> {}' .format(escolha, lista4))
+
+lista5 = range (1, 60)
+escolha = random.choice (lista5)
+print('6ª escolha --> {}' .format(escolha, lista5))
